aa40dcad29bd8f8ead0b342f54a8a3717ed7c1f2
kagg-design <info@kagg.eu>
2024-03-09 19:13:20 +0300

    Housekeeping.

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,57 +1,107 @@
+const glob = require( 'glob' );
 const path = require( 'path' );
-const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+const CssMinimizerWebpackPlugin = require( 'css-minimizer-webpack-plugin' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const TerserPlugin = require( 'terser-webpack-plugin' );
+const WebpackRemoveEmptyScriptsPlugin = require( 'webpack-remove-empty-scripts' );
 
-const webPackModule = ( production = true ) => {
+const webPackModule = ( production ) => {
 	return {
 		rules: [
 			{
-				loader: 'babel-loader',
 				test: /\.js$/,
 				exclude: /node_modules/,
-				query: {
-					presets: [ 'env' ],
+				loader: 'babel-loader',
+				options: {
+					presets: [ '@babel/preset-env' ],
 				},
 			},
 			{
 				test: /\.s?css$/,
-				use: ExtractTextPlugin.extract( {
-					fallback: 'style-loader',
-					use: [
-						{
-							loader: 'css-loader',
-							options: {
-								sourceMap: ! production,
-								minimize: production,
-							},
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							publicPath: path.join( __dirname, 'assets' ),
 						},
-						{
-							loader: 'sass-loader',
-							options: {
-								sourceMap: ! production,
-							},
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: ! production,
+							url: false,
 						},
-						{
-							loader: 'postcss-loader',
-						},
-					],
-				} ),
+					},
+				],
 			},
 		],
 	};
 };
 
+const lookup = ( lookupPath, prefix ) => {
+	const ext = path.extname( lookupPath );
+	const entries = {};
+
+	glob.sync( lookupPath ).map( ( filePath ) => {
+		if ( filePath.includes( '.min' + ext ) ) {
+			return filePath;
+		}
+
+		let filename = path.basename( filePath, ext );
+
+		if ( 'app' === filename ) {
+			filename = path.basename( path.dirname( filePath ) );
+		}
+
+		entries[ prefix + '/' + filename ] = path.resolve( filePath );
+
+		return filePath;
+	} );
+
+	return entries;
+};
+
 const notificationsRESTAPI = ( env ) => {
-	const isProduction = 'production' === env;
+	/**
+	 * @param env.production
+	 */
+	const production = env.production ? env.production : false;
+	const cssEntries = lookup( './assets/css/*.css', 'css' );
+	const jsEntries = lookup( './assets/js/*.js', 'js' );
+	const appEntries = lookup( './src/js/**/app.js', 'js/apps' );
+
+	const entries = {
+		...cssEntries,
+		...jsEntries,
+		...appEntries,
+	};
 
 	return {
-		entry: [ 'cross-fetch', './js/notificationsRESTAPI/app.js' ],
+		devtool: production ? false : 'eval-source-map',
+		entry: entries,
+		module: webPackModule( production ),
 		output: {
-			path: path.join( __dirname, '..', 'dist' ),
-			filename: path.join( 'js', 'notificationsRESTAPI', 'app.js' ),
+			path: path.join( __dirname, 'assets' ),
+			filename: ( pathData ) => {
+				return pathData.chunk.name.includes( 'apps' )
+					? '[name].js'
+					: '[name].min.js';
+			},
 		},
-		module: webPackModule( ! isProduction ),
-		plugins: [ new ExtractTextPlugin( path.join( 'css', 'sample.css' ) ) ],
-		devtool: isProduction ? '' : 'inline-source-map',
+		plugins: [
+			new WebpackRemoveEmptyScriptsPlugin(),
+			new MiniCssExtractPlugin( {
+				filename: '[name].min.css',
+			} ),
+		],
+		optimization: {
+			minimizer: [
+				new TerserPlugin( {
+					extractComments: false,
+				} ),
+				new CssMinimizerWebpackPlugin(),
+			],
+		},
 	};
 };
 
